80573bc50720e5ffc474ad708d433d91be75fa28
DevEverton <everton.mercante@gmail.com>
2018-05-15 11:34:34 -0300

    Integrate timer

--- Vale_a_pena?/Controllers/CalculateViewController.swift
+++ Vale_a_pena?/Controllers/CalculateViewController.swift
@@ -15,10 +15,11 @@ class CalculateViewController: UIViewController {
     @IBOutlet weak var resultView: UIView!
     @IBOutlet weak var daysOrHoursLabel: UILabel!
     
-    var resultInDays = 0
-    var resultInHours = 0
-    var isDays = Bool()
+    var resultInDays = 10.0
+    var resultInHours = 10.0
+    var isDays: Bool = false
     var timer = Timer()
+    var counter = 0.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,26 +34,58 @@ class CalculateViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         resultView.alpha = 0.0
         priceTextField.text = ""
+        
     }
     
     func runTimer() {
-        timer = Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: (#selector(CalculateViewController.updateTimer)), userInfo: nil, repeats: true)
+        timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: (#selector(CalculateViewController.updateTimer)), userInfo: nil, repeats: true)
         
     }
     
     @objc func updateTimer(){
+        counter += 0.1
+        if isDays {
+            daysOrHoursLabel.text = "dias"
+            if counter < resultInDays {
+                hoursLabel.text = "\(counter.rounded(toPlaces: 2))"
+            }else if counter == resultInDays {
+                counter = 0
+                timer.invalidate()
+            }
+        }else {
+            daysOrHoursLabel.text = "horas"
+            if counter < resultInHours {
+                hoursLabel.text = "\(counter.rounded(toPlaces: 2))"
+            }else if counter == resultInDays {
+                counter = 0
+                timer.invalidate()
+            }
+        }
         
     }
 
     @IBAction func calculateButtonTapped(_ sender: Any) {
         
         priceTextField.resignFirstResponder()
+
         UIView.animate(withDuration: 1.0) {
             self.resultView.alpha = 1.0
         }
-        
+        counter = 0.0
+        runTimer()
         
     }
     
 
 }
+
+extension Double {
+    /// Rounds the double to decimal places value
+    func rounded(toPlaces places:Int) -> Double {
+        let divisor = pow(10.0, Double(places))
+        return (self * divisor).rounded() / divisor
+    }
+}
+
+
+
