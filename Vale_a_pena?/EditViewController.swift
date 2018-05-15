761f00f57999f6cd0c24df8a7189dc2d86f0c36e
DevEverton <everton.mercante@gmail.com>
2018-05-14 23:04:26 -0300

    Create swift files

---
+++ Vale_a_pena?/EditViewController.swift
@@ -0,0 +1,30 @@
+//
+//  EditViewController.swift
+//  Vale_a_pena?
+//
+//  Created by Everton Carneiro on 14/05/2018.
+//  Copyright © 2018 Everton. All rights reserved.
+//
+
+import UIKit
+
+class EditViewController: UIViewController {
+    
+    @IBOutlet weak var salaryTextField: UITextField!
+    @IBOutlet weak var weeklyWorkHoursTextField: UITextField!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    @IBAction func saveButtonTapped(_ sender: Any) {
+    }
+    
+
+}
